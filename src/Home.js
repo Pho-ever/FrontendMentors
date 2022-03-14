d18bccd31bab0e6a6418656514eb58709974ca6c
Ph0-ever <oluwayomiforever@gmail.com>
2022-03-14 20:48:10 +0100

    Added functionality

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,30 @@
-const counter = 0;
-function increment() {
-    counter++;
-}
+import { useState } from "react";
+
+
 
 
 const Home = () => {
+
+    const [num, setNum] = useState(0)
+
+    const incNum = () => {
+        setNum(num + 1)
+    };
+
+    const decNum = () => {
+        setNum(num - 1)
+    }
+
+    // export default decNum;
+    // export default incNum;
+
     return (
         <div className="home">
             <div className="box">
                 <div className="likes">
-                    <i> + </i>
-                    <div> 12 </div>
-                    <i> - </i>
+                    <i onClick={incNum}> + </i>
+                    <div> {num} </div>
+                    <i onClick={decNum}> - </i>
                 </div>
                 <div className="text">
                     <div className="info">
