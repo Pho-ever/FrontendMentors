3287f758ee9966d0e92047651127fdb33e07eecb
Ph0-ever <oluwayomiforever@gmail.com>
2022-03-15 16:42:46 +0100

    Created a component

--- src/Home.js
+++ src/Home.js
@@ -1,45 +1,38 @@
-import { useState } from "react";
-
-
+import Files from './data.json'
 
 
 const Home = () => {
-
-    const [num, setNum] = useState(0)
-
-    const incNum = () => {
-        setNum(num + 1)
-    };
-
-    const decNum = () => {
-        setNum(num - 1)
-    }
-
-    // export default decNum;
-    // export default incNum;
-
     return (
         <div className="home">
-            <div className="box">
-                <div className="likes">
-                    <i onClick={incNum}> + </i>
-                    <div> {num} </div>
-                    <i onClick={decNum}> - </i>
-                </div>
-                <div className="text">
-                    <div className="info">
-                        {/* <img src="{''}" alt="" /> */}
-                        <div className="img">heyy</div>
-                        <div className="name"> Baby</div>
-                        <div className="duration"> 1 month ago</div>
-                        <div className="reply">Reply</div>
-                    </div>
-                    <div className="comment"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem ipsa iusto nesciunt iste id, dolores facere deserunt commodi obcaecati eveniet.</div>
-                </div>
+            <div className="box">{
+                Files.comments.map(file => {
+                    return (
+                        <div className="box2">
+                            <div className="likes">
+                                <i>+</i>
+                                <h2 className="num">{file.score}</h2>
+                                <i>-</i>
+                            </div>
+                            <div className="text">
+                                <div className="info">
+                                    <div className="img">{file.png}</div>
+                                    <div className="name">{file.username}</div>
+                                    <div className="duration">{file.createdAt}</div>
+                                    <div className="reply">Reply</div>
+                                </div>
+                                <div className="comment"> {file.content}</div>
+                            </div>
+                        </div>
+                    )
+                })
+            }
             </div>
         </div>
-
-    );
+    )
 }
 
 export default Home;
+
+
+
+
