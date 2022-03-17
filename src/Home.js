b6b5791c1b660eaf4ef07dcfd08efad8718d1c2a
Ph0-ever <oluwayomiforever@gmail.com>
2022-03-17 03:34:25 +0100

    Refactored

--- src/Home.js
+++ src/Home.js
@@ -3,31 +3,33 @@ import Files from './data.json'
 
 const Home = () => {
     return (
-        <div className="home">
-            <div className="box">{
-                Files.comments.map(file => {
-                    return (
-                        <div className="box2">
-                            <div className="likes">
-                                <i>+</i>
-                                <h2 className="num">{file.score}</h2>
-                                <i>-</i>
-                            </div>
-                            <div className="text">
-                                <div className="info">
-                                    <div className="img">{file.png}</div>
-                                    <div className="name">{file.username}</div>
-                                    <div className="duration">{file.createdAt}</div>
-                                    <div className="reply">Reply</div>
+        <>{
+            Files.comments.map(file => {
+                return (
+                    <div className="home">
+                        <div className="box">
+                            <div className="box2">
+                                <div className="likes">
+                                    <i>+</i>
+                                    <h2 className="num">{file.score}</h2>
+                                    <i>-</i>
                                 </div>
-                                <div className="comment"> {file.content}</div>
+                                <div className="text">
+                                    <div className="info">
+                                        <div className="img">{file.png}</div>
+                                        <div className="name">{file.username}</div>
+                                        <div className="duration">{file.createdAt}</div>
+                                        <div className="reply">Reply</div>
+                                    </div>
+                                    <div className="comment"> {file.content}</div>
+                                </div>
                             </div>
                         </div>
-                    )
-                })
-            }
-            </div>
-        </div>
+                    </div>
+                )
+            })
+        }
+        </>
     )
 }
 
