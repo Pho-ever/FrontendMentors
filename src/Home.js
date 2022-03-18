6af92af16ce90b4a977873ec58bb550057c4577e
Ph0-ever <oluwayomiforever@gmail.com>
2022-03-18 10:50:50 +0100

    updated

--- src/Home.js
+++ src/Home.js
@@ -1,23 +1,30 @@
 import Files from './data.json'
+// import { useState } from 'react'
+
+
+
+
 
 
 const Home = () => {
+
+
     return (
         <>{
-            Files.comments.map(file => {
+            Files.comments.map((file = file.id === 1) => {
                 return (
                     <div className="home">
                         <div className="box">
                             <div className="box2">
                                 <div className="likes">
-                                    <i>+</i>
+                                    <i >+</i>
                                     <h2 className="num">{file.score}</h2>
                                     <i>-</i>
                                 </div>
                                 <div className="text">
                                     <div className="info">
-                                        <div className="img">{file.png}</div>
-                                        <div className="name">{file.username}</div>
+                                        <div className="img">{ }</div>
+                                        <div className="name">{file.user.username}</div>
                                         <div className="duration">{file.createdAt}</div>
                                         <div className="reply">Reply</div>
                                     </div>
